2b6764d82207fd65aafc056ef19e7b24a17de90a
Katie Saramutina <saramutina@gmail.com>
2022-06-02 21:51:25 +0300

    Initial Commit

---
+++ Random Users/Random_UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_UsersApp.swift
+//  Random Users
+//
+//  Created by Katie Saramutina on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Random_UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
