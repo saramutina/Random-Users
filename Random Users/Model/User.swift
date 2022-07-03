57996764f80f68546d8edb66d4e90bb62e3bf039
Katie Saramutina <saramutina@gmail.com>
2022-07-03 08:57:16 +0300

    Add profile pictures

--- Random Users/Model/User.swift
+++ Random Users/Model/User.swift
@@ -18,10 +18,12 @@ struct Response: Decodable {
 struct User: Decodable, Identifiable {
     let id: String
     let name: Name
+    let picture: Picture
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         name = try values.decode(Name.self, forKey: .name)
+        picture = try values.decode(Picture.self, forKey: .picture)
         let loginInfo = try values.nestedContainer(keyedBy: LoginInfoCodingKeys.self, forKey: .login)
         id = try loginInfo.decode(String.self, forKey: .uuid)
     }
@@ -29,6 +31,7 @@ struct User: Decodable, Identifiable {
     enum CodingKeys: String, CodingKey {
         case login
         case name
+        case picture
     }
     enum LoginInfoCodingKeys: String, CodingKey {
         case uuid
@@ -47,4 +50,10 @@ struct Name: Decodable {
     let title: String
     let first: String
     let last: String
+}
+
+struct Picture: Decodable {
+    let large: String
+    let medium: String
+    let thumbnail: String
 }
